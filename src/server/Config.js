cd0e424738db985ff9f453727a7d7ed71c9f9482
Sunil Morries J <sunil.morries@tricog.com>
2026-08-25 18:23:23 +0530

    Config: bump APP_VERSION/APP_DEPLOYED_AT for the vertical-flow deploy (@112)

--- src/server/Config.js
+++ src/server/Config.js
@@ -131,6 +131,6 @@ var CONFIG = {
    * APP_DEPLOYED_AT  = when that version is pointed at the production URL.
    */
   APP_NAME: 'SIP',
-  APP_VERSION: '111',
-  APP_DEPLOYED_AT: 'Aug 25, 2026, 2:25 PM'
+  APP_VERSION: '112',
+  APP_DEPLOYED_AT: 'Aug 25, 2026, 3:40 PM'
 };
